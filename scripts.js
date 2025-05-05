f9580b60cc33461741cc8a3fe4b4c5ed081b4ac0
Ganesh Kandi <109548419+ganesh-kandi@users.noreply.github.com>
2025-05-05 17:32:08 +0530

    Add files via upload
    first commit

---
+++ scripts.js
@@ -0,0 +1,52 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
+//form fill----
+const form = document.querySelector('form[name="contact"]');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(form);
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams(formData).toString()
+        }).then(() => {
+            const successMessage = document.createElement('p');
+            successMessage.textContent = 'Thank you for your message!';
+            form.parentNode.replaceChild(successMessage, form);
+        }).catch((error) => {
+            console.error(error);
+        });
+    });
+
+
+
+/* Initializing all tooltips on the page */
+$(function () {
+    $('[data-toggle=tooltip]').tooltip();
+});
+
